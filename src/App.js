a2dce33a1f73096e7fce9147062517c7f350f9e5
kyh012412 <kyh0124.12@gmail.com>
2024-03-05 17:41:08 +0900

    input onChange로 useEffect로 확인

--- src/App.js
+++ src/App.js
@@ -4,12 +4,18 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [count, setCount] = useState(1);
+  const [name, setName] = useState('');
 
   const handleCountUpdate = () => {
     setCount(count + 1);
   };
 
+  const handleInputChange = (e) => {
+    setName(e.target.value);
+  };
+
   // 랜더링 될때마다 매번 실행됨
+  // 정확하게는 랜더링이 완료된 후에 작동되는 코드임
   useEffect(() => {
     console.log(
       '새로 랜더링이 되서 useEffect로 인해 실행된 로그출력~ count : ' + count
@@ -20,6 +26,8 @@ function App() {
     <div>
       <button onClick={handleCountUpdate}>Update</button>
       <span>count: {count}</span>
+      <input type="text" value={name} onChange={handleInputChange} />
+      <span>{name}</span>
     </div>
   );
 }
